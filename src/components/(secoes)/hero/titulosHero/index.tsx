c877591257ee37af542babf739fd7423acde1e9c
EAlvesT <106121688+EAlvesT@users.noreply.github.com>
2025-04-07 10:13:37 -0300

    Att tmnh img e not found

--- src/components/(secoes)/hero/titulosHero/index.tsx
+++ src/components/(secoes)/hero/titulosHero/index.tsx
@@ -13,10 +13,10 @@ export function TitulosHero() {
             <div className={styles.textos}>
               <h2 data-aos="fade-right">O que você vai encontrar em nossa loja:</h2>
               <ul className={styles.listaItens}>
-                <li data-aos="fade-right" data-aos-delay="600">Pneus novos de várias medidas, cargas, agro e OTR</li>
-                <li data-aos="fade-right" data-aos-delay="700">Pneus reformados com garantia</li>
-                <li data-aos="fade-right" data-aos-delay="800">Reforma de pneus, cargas, agro e OTR</li>
-                <li data-aos="fade-right" data-aos-delay="800">Serviços de borracharia</li>
+                <li data-aos="fade-right" data-aos-delay="200">Pneus novos de várias medidas, cargas, agro e OTR</li>
+                <li data-aos="fade-right" data-aos-delay="300">Pneus reformados com garantia</li>
+                <li data-aos="fade-right" data-aos-delay="400">Reforma de pneus, cargas, agro e OTR</li>
+                <li data-aos="fade-right" data-aos-delay="500">Serviços de borracharia</li>
               </ul>
             </div>
           </div>
